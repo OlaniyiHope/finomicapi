2f27c6e1139d82a912240237e8b15ca5424e99f6
LifeMirrorApp <lifemirrorapp@gmail.com>
2025-05-18 04:56:36 +0100

    all

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 app.use(
   session({
-    secret: process.env.GOOGLE_CLIENT_SECRET,
+    // secret: process.env.GOOGLE_CLIENT_SECRET,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   })
@@ -56,6 +57,13 @@ app.use("/api/auth", authRoute);
 app.use("/api/ai", aiRoute);
 
 // Use commonRouter with specific routes requiring authentication
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+});
 
 // app.use("/api/", commonRoute(s3));
 
@@ -63,9 +71,10 @@ app.use("/api/ai", aiRoute);
 // const PORT = process.env.PORT || 8000;
 // app.listen(PORT, console.log(`Server running on port ${PORT}`));
 const PORT = process.env.PORT || 8000;
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+
 app.listen(PORT, "127.0.0.1", () => {
   console.log(`Server running on port ${PORT}`);
 });
+// server.on("error", (err) => {
+//   console.error("Server failed to start:", err);
+// });
